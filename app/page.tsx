b332b3a946df6dd41d913a6fb3d69720181167df
Roshan Khadka <khadkaasd963@gmail.com>
2025-12-27 13:05:15 +0545

    setup landing page structure and base components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import Image from "next/image";
+import ContactSection from "./components/contact/ContactSection";
+import Footer from "./components/footer/Footer";
+import HeroSection from "./components/hero/HeroSection";
+import HowItWorks from "./components/how-it-works/HowItWorks";
+import Navbar from "./components/navbar/navbar";
+import WhyChooseUs from "./components/why-chose-us/WhyChoseUs";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div >
-      Fanup
-     
-    </div>
+    <>
+      <Navbar />
+      <HeroSection />
+      <WhyChooseUs />
+      <HowItWorks />
+      <ContactSection />
+      <Footer />
+    </>
   );
 }
