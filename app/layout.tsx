47b79ea4ec426540f9ed27650505c2d3c3aed2a4
Roshan Khadka <khadkaasd963@gmail.com>
2026-02-23 13:36:45 +0545

    Update global styles and root layout configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,20 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import Navbar from "./_components/navbar";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "@/context/AuthContext";
-
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        <AuthProvider>
-        {children}
-        </AuthProvider>
+      <body className={poppins.variable}>
+        <AuthProvider>{children}</AuthProvider>
         <ToastContainer position="top-right" />
       </body>
     </html>
